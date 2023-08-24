baa252fed9b248868a37a8a0fe7609653b8a5839
kh <127534515+kh5402@users.noreply.github.com>
2023-08-25 08:40:55 +0900

    Update makeRSS_Ledge.ai.py

--- makeRSS_Ledge.ai.py
+++ makeRSS_Ledge.ai.py
@@ -1,20 +1,24 @@
-from bs4 import BeautifulSoup
 import requests
-import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+import feedparser
 
 url = 'https://ledge.ai/'
 
 response = requests.get(url)
 soup = BeautifulSoup(response.content, 'html.parser')
 
-# XMLのルートを作成
-root = ET.Element("articles")
-
 # 記事へのリンクを取得
 article_links = [a['href'] for a in soup.find_all('a', href=True) if '/articles/' in a['href']]
 
 # 重複を取り除く
 article_links = list(set(article_links))
+
+print(article_links)
+
+# RSSフィードを作成
+feed = feedparser.FeedParserDict()
+feed['feed'] = {'title': 'Ledge.aiの最新記事', 'link': url, 'description': 'Ledge.aiの最新記事のRSSフィード'}
+feed['entries'] = []
 
 # 各記事から詳細を取得
 for link in article_links:
@@ -32,15 +36,17 @@ for link in article_links:
     date_tag = article_soup.find('meta', attrs={'property': 'article:published_time'})
     date = date_tag['content'] if date_tag else "日付が見つかりません"
 
-    # XMLに記事情報を追加
-    article_elem = ET.SubElement(root, "article")
-    ET.SubElement(article_elem, "title").text = title
-    ET.SubElement(article_elem, "description").text = description
-    ET.SubElement(article_elem, "url").text = link
-    ET.SubElement(article_elem, "date").text = date
+    # RSSエントリーに記事情報を追加
+    entry = {
+        'title': title,
+        'link': link,
+        'description': description,
+        'published': date
+    }
+    feed['entries'].append(entry)
 
-# XMLをファイルに保存
-tree = ET.ElementTree(root)
-tree.write("feed.xml")
+# RSSファイルに保存
+with open('feed.xml', 'w', encoding='utf-8') as file:
+    file.write(feedparser.dumps(feed))
 
-print(f"合計 {len(article_links)} 記事を取得し、XMLファイルに保存しました。")
+print(f"合計 {len(article_links)} 記事を取得し、RSSファイルに保存しました。")
