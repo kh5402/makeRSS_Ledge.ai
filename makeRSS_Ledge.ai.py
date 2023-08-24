0435417f5109c522fdd3386790c90740b145ea77
kh <127534515+kh5402@users.noreply.github.com>
2023-08-25 08:36:54 +0900

    Update makeRSS_Ledge.ai.py

--- makeRSS_Ledge.ai.py
+++ makeRSS_Ledge.ai.py
@@ -1,33 +1,46 @@
 from bs4 import BeautifulSoup
-from feedgenerator import Rss201rev2Feed
 import requests
+import xml.etree.ElementTree as ET
 
-# URL
 url = 'https://ledge.ai/'
 
-# URLからHTMLを取得
 response = requests.get(url)
-html_content = response.text
+soup = BeautifulSoup(response.content, 'html.parser')
 
-# BeautifulSoupで解析
-soup = BeautifulSoup(html_content, 'html.parser')
-print(soup)
+# XMLのルートを作成
+root = ET.Element("articles")
 
-# RSSフィードを生成
-feed = Rss201rev2Feed(
-    title="Ledge.ai Articles",
-    link=url,
-    description="Latest articles from Ledge.ai",
-)
+# 記事へのリンクを取得
+article_links = [a['href'] for a in soup.find_all('a', href=True) if '/articles/' in a['href']]
 
-# 記事のタイトルとURLを取得してフィードに追加
-for article in soup.select('article'):
-    title = article.select_one('h2').text if article.select_one('h2') else None
-    url = article.select_one('a')['href'] if article.select_one('a') else None
-    feed.add_item(title=title, link=url)
+# 重複を取り除く
+article_links = list(set(article_links))
 
-# RSSフィードをXMLファイルとして保存
-with open('feed.xml', 'w', encoding='utf-8') as file:
-    feed.write(file, 'utf-8')
+# 各記事から詳細を取得
+for link in article_links:
+    article_response = requests.get(link)
+    article_soup = BeautifulSoup(article_response.content, 'html.parser')
 
-print("RSS feed generated successfully!")
+    # タイトル
+    title = article_soup.find('title').text
+
+    # 説明
+    description_tag = article_soup.find('meta', attrs={'name': 'description'})
+    description = description_tag['content'] if description_tag else "説明が見つかりません"
+
+    # 日付
+    date_tag = article_soup.find('meta', attrs={'property': 'article:published_time'})
+    date = date_tag['content'] if date_tag else "日付が見つかりません"
+
+    # XMLに記事情報を追加
+    article_elem = ET.SubElement(root, "article")
+    ET.SubElement(article_elem, "title").text = title
+    ET.SubElement(article_elem, "description").text = description
+    ET.SubElement(article_elem, "url").text = link
+    ET.SubElement(article_elem, "date").text = date
+
+# XMLをファイルに保存
+tree = ET.ElementTree(root)
+tree.write("articles.xml")
+
+print(f"合計 {len(article_links)} 記事を取得し、XMLファイルに保存しました。")
