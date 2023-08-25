733bfb7f0e82c46e45ef6cae6446343806c2b4f7
kh <127534515+kh5402@users.noreply.github.com>
2023-08-25 15:30:20 +0900

    Update makeRSS_Ledge.ai.py

--- makeRSS_Ledge.ai.py
+++ makeRSS_Ledge.ai.py
@@ -2,51 +2,41 @@ from bs4 import BeautifulSoup
 from feedgenerator import Rss201rev2Feed
 import requests
 import datetime
+import asyncio
+from pyppeteer import launch
 
-url = 'https://ledge.ai/'
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+getURL = 'https://ledge.ai/'
 
-# 新着記事の部分を取得
-new_articles_section = soup.find('section', {'id': 'new-articles'})
+async def get_nuxt_data():
 
-# 記事のタイトルとリンクを取得
-articles = []
-for article in new_articles_section.find_all('h2'):
-    title = article.text.strip()
-    link = article.find('a')['href']
-    articles.append((title, link))
-
-
-feed = Rss201rev2Feed(
-    title='Ledge.aiの最新記事',
-    link=url,
-    description='Ledge.aiの最新記事のRSSフィード',
-)
-
-# 各記事から詳細を取得
-for title, link in articles:
-    article_response = requests.get(link)
-    article_soup = BeautifulSoup(article_response.content, 'html.parser')
-
-    # 説明
-    description_tag = article_soup.find('meta', attrs={'name': 'description'})
-    description = description_tag['content'] if description_tag else "説明が見つかりません"
-
-    # 日付
-    date_tag = article_soup.find('meta', attrs={'property': 'article:published_time'})
-    date = datetime.datetime.fromisoformat(date_tag['content']) if date_tag else "日付が見つかりません"
-
-    # RSSエントリーに記事情報を追加
-    feed.add_item(
-        title=title,
-        link=link,
-        description=description,
-        pubdate=date
+    # Pyppeteerでブラウザを開く
+    browser = await launch(
+        executablePath='/usr/bin/chromium-browser',
+        headless=True,
+        args=[
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--disable-gpu'
+        ],
     )
+        
+    page = await browser.newPage()
+    await page.goto(getURL)
 
-# RSSファイルに保存
-with open('feed.xml', 'w', encoding='utf-8') as file:
-    feed.write(file, 'utf-8')
+    # ページのHTMLを取得
+    html = await page.content()
+        
+    # BeautifulSoupで解析
+    soup = BeautifulSoup(html, 'html.parser')
 
-print(f"合計 {len(articles)} 記事を取得し、RSSファイルに保存しました。")
+    
+    # window.__NUXT__の内容を取得
+    nuxt_data = await page.evaluate('() => window.__NUXT__')
+    
+# 非同期関数を実行
+nuxt_data = asyncio.get_event_loop().run_until_complete(get_nuxt_data())
+
+# 取得したデータを使って何かする
+print(nuxt_data)
