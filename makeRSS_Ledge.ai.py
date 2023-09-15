8a346bd923ebc447bb5fbc71875e585a97de07df
kh <127534515+kh5402@users.noreply.github.com>
2023-09-16 04:19:14 +0900

    Update makeRSS_Ledge.ai.py

--- makeRSS_Ledge.ai.py
+++ makeRSS_Ledge.ai.py
@@ -1,75 +1,84 @@
 from bs4 import BeautifulSoup
 from feedgenerator import Rss201rev2Feed
-import requests
 from datetime import datetime
 import asyncio
 import json
 import re
 from pyppeteer import launch
 
-getURL = 'https://ledge.ai/categories/business/'
+# 複数のURLをリストで用意
+urls = [
+    'https://ledge.ai/categories/business/',
+    'https://ledge.ai/categories/learning/', 
+    'https://ledge.ai/categories/engineering',
+    'https://ledge.ai/categories/academic',
+    'https://ledge.ai/categories/public',
+    'https://ledge.ai/categories/entertainment'
+]
 
 async def main():
 
-    # Pyppeteerでブラウザを開く
-    browser = await launch(
-        executablePath='/usr/bin/chromium-browser',
-        headless=True,
-        args=[
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--disable-gpu'
-        ],
-    )
-        
-    page = await browser.newPage()
-    await page.goto(getURL)
+    for getURL in urls:
 
-    # ページのHTMLを取得
-    html = await page.content()
-        
-    # BeautifulSoupで解析
-    soup = BeautifulSoup(html, 'html.parser')
-    
-    # window.__NUXT__の内容を取得してJSONデータをPythonの辞書に変換
-    nuxt_data = json.loads(await page.evaluate('() => JSON.stringify(window.__NUXT__)'))
-
-    # "data"キーの中にある"articles"キーの"data"キーの値を取得
-    articles = nuxt_data["data"]["/categories/business"]["articles"]["data"]
-    
-    # RSSフィードの初期化
-    feed = Rss201rev2Feed(
-        title="Ledge.aiビジネスカテゴリ",
-        link=getURL,
-        description="Ledge.aiのビジネスカテゴリの最新記事",
-        language="ja",
-        pretty=True
-    )
-    
-    # 12個のデータを取得 ➡ 1ページに12個の記事あるから。
-    for article in articles[:12]:
-        title = article['attributes']['title']
-        date_str = article['attributes']['createdAt']
-        date_obj = datetime.fromisoformat(date_str.replace('Z', '+00:00'))
-        date_formatted = date_obj.strftime("%Y/%m/%d %H:%M")
-        url = "https://ledge.ai/articles/" + article['attributes']['slug']
-        description = re.sub(r'\[.*?\]\(.*?\)', '', article['attributes']['contents'][0]['content'])
-
-         # アイテムをフィードに追加
-        feed.add_item(
-            title=title,
-            link=url,
-            description=description,
-            pubdate=date_obj
+        # Pyppeteerでブラウザを開く
+        browser = await launch(
+            executablePath='/usr/bin/chromium-browser',
+            headless=True,
+            args=[
+                '--no-sandbox',
+                '--disable-setuid-sandbox',
+                '--disable-dev-shm-usage',
+                '--disable-accelerated-2d-canvas',
+                '--disable-gpu'
+            ],
         )
 
-    # RSSフィードをファイルに書き出し
-    with open('feed.xml', 'w') as f:
-        feed.write(f, 'utf-8')
+        page = await browser.newPage()
+        await page.goto(getURL)
 
-    print("RSSフィードが生成されました。")
+        # ページのHTMLを取得
+        html = await page.content()
+
+        # BeautifulSoupで解析
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # window.__NUXT__の内容を取得してJSONデータをPythonの辞書に変換
+        nuxt_data = json.loads(await page.evaluate('() => JSON.stringify(window.__NUXT__)'))
+
+        # "data"キーの中にある"articles"キーの"data"キーの値を取得
+        articles = nuxt_data["data"][f"/categories/{getURL.split('/')[-2]}"]["articles"]["data"]
+
+        # RSSフィードの初期化
+        feed = Rss201rev2Feed(
+            title=f"Ledge.ai {getURL.split('/')[-2]} カテゴリ",
+            link=getURL,
+            description=f"Ledge.aiの{getURL.split('/')[-2]}カテゴリの最新記事",
+            language="ja",
+            pretty=True
+        )
+
+        # 12個のデータを取得 ➡ 1ページに12個の記事あるから。
+        for article in articles[:12]:
+            title = article['attributes']['title']
+            date_str = article['attributes']['createdAt']
+            date_obj = datetime.fromisoformat(date_str.replace('Z', '+00:00'))
+            date_formatted = date_obj.strftime("%Y/%m/%d %H:%M")
+            url = "https://ledge.ai/articles/" + article['attributes']['slug']
+            description = re.sub(r'\[.*?\]\(.*?\)', '', article['attributes']['contents'][0]['content'])
+
+            # アイテムをフィードに追加
+            feed.add_item(
+                title=title,
+                link=url,
+                description=description,
+                pubdate=date_obj
+            )
+
+        # RSSフィードをファイルに書き出し
+        with open(f'feed_{getURL.split("/")[-2]}.xml', 'w') as f:
+            feed.write(f, 'utf-8')
+
+        print(f"{getURL.split('/')[-2]} カテゴリのRSSフィードが生成されました🎉")
 
 # 非同期関数を実行
 asyncio.get_event_loop().run_until_complete(main())
